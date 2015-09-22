2ca8424de28bc94860e8a49bb914a7076d953443
Kate <kateharkness@outlook.com>
2015-09-21 18:44:45 -0600

    adds buttonsto list and updated prot test

--- spec.js
+++ spec.js
@@ -9,19 +9,18 @@ describe('Concert in Iceland Page', function() {
   	});
 
   	it('should go to correct section when Nav links are clicked', function() {
-  		//expect(element(by.css('.page-scroll')).all(by.linkText('Concerts')).getAttribute('href').toEqual('#clist'));
 
         element(by.cssContainingText('.page-scroll', 'Concerts')).click();
 		expect(browser.getCurrentUrl()).toEqual("http://0.0.0.0:8080/#/clist");
+    browser.driver.sleep(1000);
         element(by.cssContainingText('.page-scroll', 'About')).click();
 		expect(browser.getCurrentUrl()).toEqual("http://0.0.0.0:8080/#/aboutisc");
+    browser.driver.sleep(1000);
         element(by.cssContainingText('.page-scroll', 'Contact')).click();
 		expect(browser.getCurrentUrl()).toEqual("http://0.0.0.0:8080/#/contact");
+    browser.driver.sleep(1000);
 
 	});
-
-
-
 });
 
 
